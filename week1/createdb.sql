05175d528638700c6ec5e24d05ae2208348bf954
waqas-on-github <waqasvu892@gmail.com>
2022-08-26 13:08:24 +0500

    create tabels

--- week1/createdb.sql
+++ week1/createdb.sql
@@ -1,6 +1,19 @@
--- Active: 1661364557490@@127.0.0.1@3306@mysql
-CREATE DATABASE IF NOT EXISTS sales ;
-DROP DATABASE sales;
-SHOW DATABASES;
-USE information_schema;
-SHOW TABLES;
+-- Active: 1661364557490@@127.0.0.1@3306@mysqlclea
+SHOW DATABASES;  
+DROP  DATABASE sales;
+
+CREATE DATABASE students;
+USE students;
+
+CREATE TABLE  studentdata
+ ( stuid int, stuname VARCHAR(50), stuage int, stucity VARCHAR(30));
+ 
+INSERT INTO studentdata (stuid  ,stuname ,stuage, stucity )
+    VALUES (1,'hamza' ,22,'karachi'),
+           (2,'noman' ,23, 'lahore')
+           ;
+DROP TABLE studentdata;
+
+SELECT * FROM studentdata;
+
+ALTER TABLE studentdata 
